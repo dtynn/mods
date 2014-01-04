76e5981d9d4deab270136d2df540d5519090ed32
dtynn <dtynn@163.com>
2014-01-04 13:02:07 +0800

    first

---
+++ mAuth/mAuth.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+
+class miniAuth(object):
+    def __init__(self, connRead, connWrite):
+        self.connRead = connRead
+        self.connWrite = connWrite
+        return
+
+    def tableInit(self):
+        return
+
+    def userNew(self):
+        return
